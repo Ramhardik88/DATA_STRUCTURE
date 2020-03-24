118476597cc226b04a6b05099e35eb59ee86bedb
Ram Kumar <Ram Kumar@DESKTOP-L9ESGLB>
2020-03-24 10:24:11 +0530

    Linked list push

---
+++ example/src/example/Node.java
@@ -0,0 +1,7 @@
+package example;
+
+public class Node {
+	int data;
+	Node next;
+
+}
---
+++ example/src/example/Runner.java
@@ -0,0 +1,25 @@
+package example;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList list = new LinkedList();
+		list.insert(20);
+		list.insert(45);
+		list.insert(60);
+		
+		list.insert(10);
+		list.InsertAtstart(35);
+		list.InsertAt(4, 50);
+		
+		list.deteteAt(3);
+		
+		
+		
+		list.show();
+		
+
+	}
+
+}
